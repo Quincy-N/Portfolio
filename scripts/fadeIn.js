043443cdb148fe8f8f7e91e9013075b2db352f17
Quincy-N <njemqnd@aol.com>
2024-04-09 15:36:11 -0700

    fix fade in to be resposive

--- scripts/fadeIn.js
+++ scripts/fadeIn.js
@@ -1,9 +1,27 @@
 const sections = document.querySelectorAll('section');
 let sectionChildren = [];
-for (let section of sections) {
-    for (let i = 0; i < section.children.length; i++)
-    sectionChildren.push(section.children[i]);
+let sectionHeights = [];
+let minSectionHeight;
+function getMinSectionHeight() {
+    for (let i = 0; i < sections.length; i++) {
+        sectionHeights.push(sections[i].getBoundingClientRect().height);
 }
+    for (let i = 1; i < sectionHeights.length; i++) {
+        if (sectionHeights[i] < sectionHeights[i-1]) {
+            minSectionHeight = sectionHeights[i];
+        }
+    }
+}
+getMinSectionHeight();
+console.log(minSectionHeight);
+for (let section of sections) {
+    for (let i = 0; i < section.children.length; i++) {
+        sectionChildren.push(section.children[i]);
+    }
+}
+window.addEventListener('resize', () => {
+    getMinSectionHeight();
+})
 const sectionObserver = new IntersectionObserver(function(entries, sectionObserver) {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -11,7 +29,7 @@ const sectionObserver = new IntersectionObserver(function(entries, sectionObserv
             sectionObserver.unobserve(entry.target);
         }
     })
-}, {threshold: 0, rootMargin: '0px 0px -50px 0px'});
+}, {threshold: 0, rootMargin: `0px 0px -${minSectionHeight/3}px 0px`});
 
 for (let sectionChild of sectionChildren) {
     sectionObserver.observe(sectionChild);
